d9af3a88220a70f833472ff9276619ce7ab6a479
mathiaskim522 <hjjam100@gmail.com>
2025-04-18 16:18:34 +0900

    feat: update package.json to use scoped name and add @nestjs/swagger dependency

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Catarchy API')
+    .setDescription('Catarchy APIs')
+    .setVersion('alpha')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
