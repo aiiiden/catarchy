4abdcaa02a6aa947192eebd7a8a8ffda51e13a23
jw-r <evencoding@gmail.com>
2025-05-08 14:45:18 +0900

    feat: scene 3

--- packages/frontend/src/routes/story/$storyId.tsx
+++ packages/frontend/src/routes/story/$storyId.tsx
@@ -20,6 +20,8 @@ function RouteComponent() {
     <div className="absolute inset-0">
       {search.scene === 1 && <Scene1 />}
       {search.scene === 2 && <Scene2 />}
+      {search.scene === 3 && <Scene3 />}
+      {search.scene === 4 && <Scene4 />}
     </div>
   );
 }
@@ -27,14 +29,29 @@ function RouteComponent() {
 const SceneLayout = ({
   top,
   bottom,
+  onNext,
 }: {
   top: React.ReactNode;
   bottom: React.ReactNode;
+  onNext?: () => void;
 }) => {
   return (
     <div className="p-[30px] inset-0 absolute pb-0 grid grid-rows-[230fr_308fr]">
       <div className="mx-auto pb-6 mt-auto">{top}</div>
       <div className="flex-center text-center">{bottom}</div>
+      {onNext && (
+        <div
+          className="inset-0 absolute bg-transparent"
+          onClick={onNext}
+          style={{
+            animation: 'blink 1s infinite',
+          }}
+        >
+          <p className="absolute bottom-6 right-1/2 translate-x-1/2">
+            Tab to continue
+          </p>
+        </div>
+      )}
     </div>
   );
 };
@@ -67,6 +84,11 @@ const Scene1 = () => {
 };
 
 const Scene2 = () => {
+  const { params } = useMatch({
+    from: '/story/$storyId',
+  });
+  const navigate = useNavigate();
+
   const lines = [
     "I've always wanted to have a cat",
     'I was envious of cats basking in the sun, lazily grooming themselves.',
@@ -74,14 +96,15 @@ const Scene2 = () => {
   ];
 
   const [currentIdx, setCurrentIdx] = useState(0);
+  const [readyToTab, setReadyToTab] = useState(false);
 
   return (
     <SceneLayout
       top={
         <img
-          src="/images/story1_scene1.png"
+          src="/images/story1_scene2.png"
           className="w-[138px] mx-auto"
-          alt="scene1"
+          alt="scene2"
         />
       }
       bottom={
@@ -96,6 +119,10 @@ const Scene2 = () => {
                   setTimeout(() => {
                     setCurrentIdx(i + 1);
                   }, 1000);
+                } else if (i === lines.length - 1) {
+                  setTimeout(() => {
+                    setReadyToTab(true);
+                  }, 500);
                 }
               }}
             >
@@ -103,6 +130,148 @@ const Scene2 = () => {
             </Typing>
           ))}
         </div>
+      }
+      onNext={
+        readyToTab
+          ? () =>
+              navigate({
+                to: '/story/$storyId',
+                params: { storyId: params.storyId },
+                search: {
+                  scene: 3,
+                },
+                replace: true,
+              })
+          : undefined
+      }
+    />
+  );
+};
+
+const Scene3 = () => {
+  const { params } = useMatch({
+    from: '/story/$storyId',
+  });
+  const navigate = useNavigate();
+
+  const lines = [
+    'But had my life been cursed?',
+    'I was run over by a drunk-driving truck while walking down the street.',
+  ];
+
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [readyToTab, setReadyToTab] = useState(false);
+
+  return (
+    <SceneLayout
+      top={
+        <img
+          src="/images/story1_scene3.png"
+          className="w-[133px] mx-auto"
+          alt="scene3"
+        />
+      }
+      bottom={
+        <div className="space-y-4">
+          {lines.slice(0, currentIdx + 1).map((line, i) => (
+            <Typing
+              key={i}
+              speed={60}
+              startDelay={i === 0 ? 500 : 0}
+              onEnd={() => {
+                if (i < lines.length - 1) {
+                  setTimeout(() => {
+                    setCurrentIdx(i + 1);
+                  }, 1000);
+                } else if (i === lines.length - 1) {
+                  setTimeout(() => {
+                    setReadyToTab(true);
+                  }, 500);
+                }
+              }}
+            >
+              {line}
+            </Typing>
+          ))}
+        </div>
+      }
+      onNext={
+        readyToTab
+          ? () =>
+              navigate({
+                to: '/story/$storyId',
+                params: { storyId: params.storyId },
+                search: {
+                  scene: 4,
+                },
+                replace: true,
+              })
+          : undefined
+      }
+    />
+  );
+};
+
+const Scene4 = () => {
+  const { params } = useMatch({
+    from: '/story/$storyId',
+  });
+  const navigate = useNavigate();
+
+  const lines = [
+    'Where... am I...?',
+    "Ah... I'm dead...",
+    'I must be in the afterlife!\nEverything around me is pitch black and silent.',
+  ];
+
+  const [currentIdx, setCurrentIdx] = useState(0);
+  const [readyToTab, setReadyToTab] = useState(false);
+
+  return (
+    <SceneLayout
+      top={
+        <img
+          src="/images/story1_scene4.png"
+          className="w-[138px] mx-auto"
+          alt="scene4"
+        />
+      }
+      bottom={
+        <div className="space-y-4">
+          {lines.slice(0, currentIdx + 1).map((line, i) => (
+            <Typing
+              key={i}
+              speed={60}
+              startDelay={i === 0 ? 500 : 0}
+              onEnd={() => {
+                if (i < lines.length - 1) {
+                  setTimeout(() => {
+                    setCurrentIdx(i + 1);
+                  }, 1000);
+                } else if (i === lines.length - 1) {
+                  setTimeout(() => {
+                    setReadyToTab(true);
+                  }, 500);
+                }
+              }}
+            >
+              {line}
+            </Typing>
+          ))}
+        </div>
+      }
+      onNext={
+        readyToTab
+          ? () =>
+              navigate({
+                to: '/story/$storyId',
+                params: { storyId: params.storyId },
+                search: {
+                  scene: 5,
+                },
+                replace: true,
+              })
+          : undefined
       }
     />
   );
